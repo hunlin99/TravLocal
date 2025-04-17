56e4e3837c955654979ad254005e00d5cbf9bd42
Ling Chang <127913579+hunlin99@users.noreply.github.com>
2025-04-17 08:51:34 +0000

    add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import ProfileSection from "@/components/ProfileSection";
 import HeroSection from "@/components/HeroSection";
 import FAQsection from "@/components/FAQsection";
 import ActionRevealSection from "@/components/ActionRevealSection";
+import Footer from "@/components/footer";
 
 
 const Page: React.FC = () => {
@@ -42,22 +43,7 @@ const Page: React.FC = () => {
       <FAQsection />
 
       {/* Footer Section */}
-      <section className="bg-dark p-5 vh-50">
-        <div className="container d-flex justify-content-between">
-          <div className="py-5 col-3">
-            <div className="d-grid gap-3 text-white">
-              <h3>Download <br /> TravLocal app</h3>
-              <a className="btn btn-outline-light btn-lg px-4" href="#">App Store</a>
-              <a className="btn btn-outline-light btn-lg px-4" href="#">Google Play</a>
-            </div>
-          </div>
-          <div className="py-5 col-9 text-end">
-            <a href="#" className="text-white mx-3">About Us</a>
-            <a href="#" className="text-white mx-3">FAQ</a>
-            <a href="#" className="text-white mx-3">Contact</a>
-          </div>
-        </div>
-      </section>
+      <Footer />
     </>
   );
 };
---
+++ src/components/footer.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <section className="FooterSection bg-dark p-5 vh-50">
+      <div className="container d-flex justify-content-between">
+        <div className="py-5 col-3">
+          <div className="d-grid gap-3 text-white">
+            <h3>Download <br /> TravLocal app</h3>
+            <a className="btn btn-outline-light btn-lg px-4" href="#">App Store</a>
+            <a className="btn btn-outline-light btn-lg px-4" href="#">Google Play</a>
+          </div>
+        </div>
+        <div className="py-5 col-9 text-end">
+          <a href="#" className="text-white mx-3">About Us</a>
+          <a href="#" className="text-white mx-3">FAQ</a>
+          <a href="#" className="text-white mx-3">Contact</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
